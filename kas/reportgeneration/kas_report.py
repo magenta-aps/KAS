7dc9c6328fda5d15583de80c78940ca594bca326
Mads Møller Johansen <mmj@magenta.dk>
2021-02-08 11:13:52 -0300

    41252: refactor

--- kas/reportgeneration/kas_report.py
+++ kas/reportgeneration/kas_report.py
@@ -5,154 +5,162 @@ import sys
 from fpdf import FPDF
 
 
-modtagernavn = 'Mads Møller Johansen'
-modtagernadd = 'Sanamut aqqut 21, lejl 102'
-modtagerpostnummer = '3900 Nuuk'
+class TaxPDF(FPDF):
 
-afsnavn = 'Skattestyrelsen'
-afsadd = 'Postboks 1605'
-afspostnummer = '3900 Nuuk'
+    person_number = '-'
+    reciever_name = '-'
+    reciever_address = '-'
+    reciever_postnumber = '-'
+    sender_name = '-'
+    sender_address = '-'
+    sender_postnumber = '-'
+    nemid_kode = '-'
 
-personnummer = '1234567890'
-nemidkode = '1234'
+    def set_parameters(self, person_number='-', reciever_name='', reciever_address='', reciever_postnumber='', sender_name='',
+                       sender_address='', sender_postnumber='', nemid_kode=''):
+        self.person_number = person_number
+        self.reciever_name = reciever_name
+        self.reciever_address = reciever_address
+        self.reciever_postnumber = reciever_postnumber
+        self.sender_name = sender_name
+        self.sender_address = sender_address
+        self.sender_postnumber = sender_postnumber
+        self.nemid_kode = nemid_kode
 
-pdf = FPDF()
-pdf.add_page()
+    def footer(self):
+        self.set_xy(15.0, self.h - 30)
+        self.cell(ln=0, h=5.0, align='C', w=30.0, txt=self.person_number, border=0)
 
-pdf.set_font('helvetica', 'B', 15.0)
-pdf.set_xy(105.0, 8.0)
-pdf.cell(ln=0, h=22.0, align='R', w=75.0, txt='Bilag til S1/S1U', border=0)
+    def print_tax_slip(self):
 
-pdf.set_font('helvetica', '', 13.0)
-pdf.set_xy(20.0, 8.0)
-pdf.cell(ln=0, h=22.0, align='L', w=75.0, txt='Bilag til Selvangivelse for 2019', border=0)
+        self.add_page()
 
-pdf.set_font('helvetica', '', 10.0)
-pdf.set_xy(20.0, 12.0)
-pdf.cell(ln=0, h=22.0, align='L', w=75.0, txt='Bilag vedrørende beskatning af visse kapitalafkast', border=0)
-pdf.set_xy(20.0, 15.0)
-pdf.cell(ln=0, h=22.0, align='L', w=75.0, txt='Indsendelsesfrist senest 1. maj 2020', border=0)
+        self.set_font('helvetica', 'B', 15.0)
+        self.set_xy(105.0, 8.0)
+        self.cell(ln=0, h=22.0, align='R', w=75.0, txt='Bilag til S1/S1U', border=0)
 
+        self.set_font('helvetica', '', 13.0)
+        self.set_xy(20.0, 8.0)
+        self.cell(ln=0, h=22.0, align='L', w=75.0, txt='Bilag til Selvangivelse for 2019', border=0)
 
-pdf.set_font('helvetica', '', 12.0)
+        self.set_font('helvetica', '', 10.0)
+        self.set_xy(20.0, 12.0)
+        self.cell(ln=0, h=22.0, align='L', w=75.0, txt='Bilag vedrørende beskatning af visse kapitalafkast', border=0)
+        self.set_xy(20.0, 15.0)
+        self.cell(ln=0, h=22.0, align='L', w=75.0, txt='Indsendelsesfrist senest 1. maj 2020', border=0)
 
-spacing = 1.5
-col_width = pdf.w / 3.25
-row_height = pdf.font_size
-item = "bla bla bla"
+        self.set_font('helvetica', '', 12.0)
 
-# Adressing reciever
-pdf.set_xy(30.0, 37.0)
-pdf.multi_cell(60, 5, border=0, txt=modtagernavn+"\n"+modtagernadd+"\n"+modtagerpostnummer)
+        # Adressing reciever
+        self.set_xy(30.0, 37.0)
+        self.multi_cell(60, 5, border=0, txt=self.reciever_name+"\n"+self.reciever_address+"\n"+self.reciever_postnumber)
 
+        # Adressing department
+        self.set_xy(30.0, 57.0)
+        self.multi_cell(60, 5, border=0, txt=self.sender_name+"\n"+self.sender_address+"\n"+self.sender_postnumber)
+        self.line(30, 57, 90, 72)
+        self.line(30, 72, 90, 57)
 
-# Adressing department
-pdf.set_xy(30.0, 57.0)
-pdf.multi_cell(60, 5, border=0, txt=afsnavn+"\n"+afsadd+"\n"+afspostnummer)
-pdf.line(30, 57, 90, 72)
-pdf.line(30, 72, 90, 57)
+        self.set_xy(100.0, 27.0)
+        self.rotate = 10
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='Personnummer: ', border=1)
+        self.set_xy(100.0, 32.0)
+        self.cell(ln=1, h=5.0, align='L', w=40.0, txt='Kapitalafkastskat: ', border=1)
 
+        self.set_xy(140.0, 27.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt=self.person_number, border=1)
+        self.set_xy(140.0, 32.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='15,3%', border=1)
+        self.set_xy(100.0, 37.0)
+        self.cell(ln=0, h=5.0, align='L', w=80.0, txt='Selvbetjeningsinformation', border=1)
 
-pdf.set_xy(100.0, 27.0)
-pdf.rotate = 10
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='Personnummer: ', border=1)
-pdf.set_xy(100.0, 32.0)
-pdf.cell(ln=1, h=5.0, align='L', w=40.0, txt='Kapitalafkastskat: ', border=1)
+        self.set_xy(100.0, 42.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='Tast selv internet', border=1)
+        self.set_xy(100.0, 47.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='www.silisivik.gl', border=1)
+        self.set_xy(100.0, 52.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='www.aka.gl', border=1)
 
-pdf.set_xy(140.0, 27.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt=personnummer, border=1)
-pdf.set_xy(140.0, 32.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='15,3%', border=1)
-pdf.set_xy(100.0, 37.0)
-pdf.cell(ln=0, h=5.0, align='L', w=80.0, txt='Selvbetjeningsinformation', border=1)
+        self.set_xy(140.0, 42.0)
+        self.cell(ln=0, h=5.0, align='L', w=40.0, txt='Tast selv kode', border=1)
+        self.set_xy(140.0, 47.0)
+        self.cell(ln=1, h=5.0, align='L', w=40.0, txt=self.nemid_kode, border=1)
+        self.set_xy(100.0, 52.0)
 
-pdf.set_xy(100.0, 42.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='Tast selv internet', border=1)
-pdf.set_xy(100.0, 47.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='www.silisivik.gl', border=1)
-pdf.set_xy(100.0, 52.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='www.aka.gl', border=1)
+        self.set_xy(15.0, 95.0)
 
-pdf.set_xy(140.0, 42.0)
-pdf.cell(ln=0, h=5.0, align='L', w=40.0, txt='Tast selv kode', border=1)
-pdf.set_xy(140.0, 47.0)
-pdf.cell(ln=1, h=5.0, align='L', w=40.0, txt=nemidkode, border=1)
-pdf.set_xy(100.0, 52.0)
+        self.cell(ln=0, h=5.0, align='L', w=170.0,
+                  txt='Vejledning til bilag vedrørende beskatning af visse kapitalafkast', border=1)
 
-pdf.set_xy(15.0, 95.0)
+        yposition = 100
+        elementheight = 40
+        self.set_xy(15.0, yposition)
+        self.rect(15.0, yposition, 170.0, elementheight)
+        self.multi_cell(170, 5, "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out "
+                                "print, graphic or web designs. The passage is attributed to an unknown typesetter in "
+                                "the 15th century who is thought to have scrambled parts of Cicero's De Finibus "
+                                "Bonorum et Malorum for use in a type specimen book.", 0)
+        yposition += elementheight
 
-pdf.cell(ln=0, h=5.0, align='L', w=170.0, txt='Vejledning til bilag vedrørende beskatning af visse kapitalafkast',
-         border=1)
+        elementheight = 15
+        self.set_xy(15.0, yposition)
+        self.rect(15.0, yposition, 170.0, elementheight)
+        self.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla bla "
+                                "bla bla bla bla bla bla bla bla bla bla", 0)
+        yposition += elementheight
 
-yposition = 100
-elementheight = 40
-pdf.set_xy(15.0, yposition)
-pdf.rect(15.0, yposition, 170.0, elementheight)
-pdf.multi_cell(170, 5, "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, "
-                       "graphic or web designs. The passage is attributed to an unknown typesetter in the 15th "
-                       "century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum "
-                       "for use in a type specimen book.", 0)
-yposition += elementheight
+        elementheight = 30
+        self.set_xy(15.0, yposition)
+        self.rect(15.0, yposition, 170.0, elementheight)
+        self.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla "
+                                "bla bla bla bla bla bla bla bla bla bla bla", 0)
+        yposition += elementheight
 
-elementheight = 15
-pdf.set_xy(15.0, yposition)
-pdf.rect(15.0, yposition, 170.0, elementheight)
-pdf.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "
-                       "bla bla bla bla bla bla bla bla", 0)
-yposition += elementheight
+        elementheight = 15
+        self.set_xy(15.0, yposition)
+        self.rect(15.0, yposition, 170.0, elementheight)
+        self.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla "
+                                "bla bla bla bla bla bla bla bla bla bla bla", 0)
+        yposition += elementheight
 
-elementheight = 30
-pdf.set_xy(15.0, yposition)
-pdf.rect(15.0, yposition, 170.0, elementheight)
-pdf.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "
-                       "bla bla bla bla bla bla bla bla", 0)
-yposition += elementheight
+        self.set_font('helvetica', '', 10.0)
+        yposition += 15
 
-elementheight = 15
-pdf.set_xy(15.0, yposition)
-pdf.rect(15.0, yposition, 170.0, elementheight)
-pdf.multi_cell(170, 5, "Hvis de er uneig i indholdet bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "
-                       "bla bla bla bla bla bla bla bla", 0)
-yposition += elementheight
+        elementheight = 5
 
+        self.set_xy(80.0, yposition)
+        self.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Beløb i kroner', border=0)
+        self.set_xy(120.0, yposition)
+        self.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Beløb i kroner', border=0)
+        self.set_xy(160.0, yposition)
+        self.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Felt nr.', border=0)
 
-pdf.set_font('helvetica', '', 10.0)
-yposition += 15
+        yposition += elementheight
 
-elementheight = 5
+        yposition += 10
 
-pdf.set_xy(80.0, yposition)
-pdf.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Beløb i kroner', border=0)
-pdf.set_xy(120.0, yposition)
-pdf.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Beløb i kroner', border=0)
-pdf.set_xy(160.0, yposition)
-pdf.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Felt nr.', border=0)
+        self.set_xy(15.0, yposition)
+        self.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Kapitalafkast  PBL (DK) 	§ 53 A', border=0)
 
-yposition += elementheight
+        self.set_xy(160.0, yposition)
+        self.cell(ln=0, h=elementheight, align='L', w=75.0, txt='201', border=0)
 
-yposition += 10
+        yposition += elementheight
 
-pdf.set_xy(15.0, yposition)
-pdf.cell(ln=0, h=elementheight, align='L', w=75.0, txt='Kapitalafkast  PBL (DK) 	§ 53 A', border=0)
+        self.line(15, yposition, 60, yposition)
+        self.line(80, yposition, 110, yposition)
+        self.line(120, yposition, 150, yposition)
 
-pdf.set_xy(160.0, yposition)
-pdf.cell(ln=0, h=elementheight, align='L', w=75.0, txt='201', border=0)
-
-yposition += elementheight
-
-pdf.line(15, yposition, 60, yposition)
-pdf.line(80, yposition, 110, yposition)
-pdf.line(120, yposition, 150, yposition)
-
-
-pdf.set_xy(15.0, pdf.h - 30)
-pdf.cell(ln=0, h=5.0, align='C', w=30.0, txt=personnummer, border=0)
-
-
-pdf.output('./invoice.pdf', 'F')
+        self.output('./invoice.pdf', 'F')
 
 
 if sys.platform.startswith("linux"):
     os.system("xdg-open ./invoice.pdf")
 else:
     os.system("./invoice.pdf")
+
+
+foo = TaxPDF()
+foo.set_parameters('1234567890', 'Mads Møller Johansen', 'Sanamut aqqut 21, lejl 102',
+                   '3900 Nuuk', 'Skattestyrelsen', 'Postboks 1605', '3900 Nuuk', '1234')
+foo.print_tax_slip()
