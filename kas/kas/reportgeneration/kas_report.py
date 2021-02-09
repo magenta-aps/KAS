c1c4a267ec52bb0bc2df635e50211484aea96cde
Mads Møller Johansen <mmj@magenta.dk>
2021-02-09 15:01:55 -0300

    41252: support dynamic amount of policies

--- kas/kas/reportgeneration/kas_report.py
+++ kas/kas/reportgeneration/kas_report.py
@@ -15,6 +15,10 @@ class TaxPDF(FPDF):
     contact_info_table_x_pos = 100.0
     contact_info_table_y_pos = 27.0
 
+    address_field_width = 70
+    address_field_x_pos = 17
+    address_field_y_pos = 37
+
     tax_year = '-'
     tax_return_date_limit = '-'
     person_number = '-'
@@ -25,10 +29,11 @@ class TaxPDF(FPDF):
     sender_address = '-'
     sender_postnumber = '-'
     nemid_kode = '-'
+    policys = ['']
 
     def set_parameters(self, tax_year='-', tax_return_date_limit='', person_number='-', reciever_name='',
                        reciever_address='', reciever_postnumber='', sender_name='',
-                       sender_address='', sender_postnumber='', nemid_kode=''):
+                       sender_address='', sender_postnumber='', nemid_kode='', policys=['']):
         self.tax_year = tax_year
         self.tax_return_date_limit = tax_return_date_limit
         self.person_number = person_number
@@ -39,24 +44,25 @@ class TaxPDF(FPDF):
         self.sender_address = sender_address
         self.sender_postnumber = sender_postnumber
         self.nemid_kode = nemid_kode
+        self.policys = policys
+
+    def header(self):
+        self.yposition = 40
+        self.set_xy(self.left_margin, self.yposition)
 
     def footer(self):
         self.set_xy(self.left_margin, self.h - 30)
         self.cell(h=5.0, align='C', w=30.0, txt=self.person_number, border=0)
-
-    def add_page(self):
-        super().add_page()
-        self.yposition = 40
+        self.set_xy(self.left_margin, self.yposition)
 
     def print_tax_slip(self):
-
         self.add_page()
 
-        self.set_font('helvetica', 'B', 15.0)
+        self.set_font('arial', 'B', 15.0)
         self.set_xy(105.0, 8.0)
         self.cell(h=self.contact_info_table_cell_height, align='R', w=75.0, txt='Bilag til S1/S1U', border=0)
 
-        self.set_font('helvetica', '', 13.0)
+        self.set_font('arial', '', 13.0)
         self.set_xy(20.0, 8.0)
         self.cell(h=self.contact_info_table_cell_height, align='L', w=75.0,
                   txt='Bilag til Selvangivelse for '+self.tax_year, border=0)
@@ -68,17 +74,18 @@ class TaxPDF(FPDF):
         self.cell(h=self.contact_info_table_cell_height, align='L', w=75.0,
                   txt='Indsendelsesfrist senest '+self.tax_return_date_limit, border=0)
 
-        self.set_font('helvetica', '', 10)
+        self.set_font('arial', '', 8.5)
         # Adressing reciever
-        self.set_xy(30.0, 37.0)
-        self.multi_cell(60, 5, border=0,
+        self.set_xy(self.address_field_x_pos, self.address_field_y_pos)
+        self.multi_cell(self.address_field_width, 3, border=0,
                         txt=self.reciever_name+"\n"+self.reciever_address+"\n"+self.reciever_postnumber)
 
         # Adressing department
-        self.set_xy(30.0, 57.0)
-        self.multi_cell(60, 5, border=0, txt=self.sender_name+"\n"+self.sender_address+"\n"+self.sender_postnumber)
-        self.line(30, 57, 90, 72)
-        self.line(30, 72, 90, 57)
+        self.set_xy(self.address_field_x_pos, self.address_field_y_pos+12)
+        self.multi_cell(self.address_field_width, 3, border=0,
+                        txt=self.sender_name+"\n"+self.sender_address+"\n"+self.sender_postnumber)
+        self.line(self.address_field_x_pos, 49, self.address_field_x_pos+self.address_field_width-30, 59)
+        self.line(self.address_field_x_pos, 59, self.address_field_x_pos+self.address_field_width-30, 49)
 
         self.set_xy(self.contact_info_table_x_pos, self.contact_info_table_y_pos)
         self.cell(h=self.contact_info_table_cell_height, align='L', w=self.contact_info_table_cell_width,
@@ -158,6 +165,8 @@ class TaxPDF(FPDF):
 
         elementheight = 5
 
+        self.set_xy(15.0, self.yposition)
+        self.cell(h=elementheight, align='L', w=75.0, txt='Kapitalafkast PBL (DK) § 53 A', border=0)
         self.set_xy(80.0, self.yposition)
         self.cell(h=elementheight, align='L', w=75.0, txt='Beløb i kroner', border=0)
         self.set_xy(120.0, self.yposition)
@@ -168,18 +177,20 @@ class TaxPDF(FPDF):
 
         self.yposition += 10
 
+        for policy in self.policys:
+            self.set_xy(self.left_margin, self.yposition)
+            self.cell(h=elementheight, align='L', w=75.0, txt=policy, border=0)
+
+            self.set_xy(160.0, self.yposition)
+            self.cell(h=elementheight, align='L', w=75.0, txt='201', border=0)
+            self.yposition += elementheight
+
+            self.line(80, self.yposition, 110, self.yposition)
+            self.line(120, self.yposition, 150, self.yposition)
+            self.yposition += 20
+
         self.set_xy(self.left_margin, self.yposition)
-        self.cell(h=elementheight, align='L', w=75.0, txt='Kapitalafkast  PBL (DK) 	§ 53 A', border=0)
-
-        self.set_xy(160.0, self.yposition)
-        self.cell(h=elementheight, align='L', w=75.0, txt='201', border=0)
-        self.yposition += elementheight
-
-        self.line(15, self.yposition, 60, self.yposition)
-        self.line(80, self.yposition, 110, self.yposition)
-        self.line(120, self.yposition, 150, self.yposition)
-        self.yposition += 20
-
+        self.cell(h=elementheight, align='L', w=75.0, txt='', border=0)
         self.set_xy(self.left_margin, self.yposition)
         self.cell(h=elementheight, align='L', w=75.0, txt='Kapitalafkastskat', border=0)
 
@@ -192,8 +203,9 @@ class TaxPDF(FPDF):
         self.line(120, self.yposition+5, 150, self.yposition+5)
         self.set_xy(160.0, self.yposition)
         self.cell(h=elementheight, align='L', w=75.0, txt='205', border=0)
+        self.yposition += elementheight
 
-        self.add_page()
+        self.yposition += 20
 
         elementheight = 15
         self.set_xy(self.left_margin, self.yposition)
@@ -257,7 +269,8 @@ class TaxPDF(FPDF):
 def main():
     tax_slip = TaxPDF()
     tax_slip.set_parameters("2020", '1. maj 2020', '1234567890', 'Mads Møller Johansen', 'Sanamut aqqut 21, lejl 102',
-                            '3900 Nuuk', 'Skattestyrelsen', 'Postboks 1605', '3900 Nuuk', '1234')
+                            '3900 Nuuk', 'Skattestyrelsen', 'Postboks 1605', '3900 Nuuk', '1234',
+                            ['ATP-12345678', 'PFA-12345678', 'Something else-12345678'])
     tax_slip.print_tax_slip()
     if sys.platform.startswith("linux"):
         os.system("xdg-open ./invoice.pdf")
