a40b59e59383e6a5c52f772d3fdc0d67a969f6b1
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2021-03-09 14:00:42 +0000

    [#41796] job list and creation flow

--- kas/kas/models.py
+++ kas/kas/models.py
@@ -14,6 +14,10 @@ from simple_history.models import HistoricalRecords
 
 class HistoryMixin(object):
 
+    UNCHANGED = 0
+    UPDATED = 1
+    CREATED = 2
+
     """
     :param data: dict to populate model instance
     :param keys: keys in dict that define how to look for an existing instance. KvPs in data are extracted by keys for the lookup
@@ -21,6 +25,7 @@ class HistoryMixin(object):
     """
     @classmethod
     def update_or_create(cls, data, *keys):
+        status = HistoryMixin.UNCHANGED
         try:
             item = cls.objects.get(**{k: v for k, v in data.items() if k in keys})
             existing_dict = model_to_dict(item)
@@ -34,11 +39,13 @@ class HistoryMixin(object):
                     setattr(item, k, v)
                 item._change_reason = "Updated by import"
                 item.save()
+                status = HistoryMixin.UPDATED
         except cls.DoesNotExist:
             item = cls(**data)
             item.change_reason = "Created by import"
             item.save()
-        return item
+            status = HistoryMixin.CREATED
+        return item, status
 
 
 class PensionCompany(models.Model):
