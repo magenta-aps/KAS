3f57eaddb5df2abc81c7d90da7391064eba0459c
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2021-03-12 11:13:21 +0100

    needed settings and changes for djangos storage backend

--- kas/project/settings.py
+++ kas/project/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 UPLOAD_PATH = '/uploads'
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
-MEDIA_URL = "/uploads/"
+MEDIA_ROOT = "/srv/media"
+MEDIA_URL = "/media/"
 
 LOGGING = {
     'version': 1,
