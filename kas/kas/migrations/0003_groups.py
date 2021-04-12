256280d16a63998eff923ec5030f8151770ff73f
Lars Peter Thomsen <larsp@magenta.dk>
2021-04-12 13:11:19 +0200

    [#42845] Setup group permissions

--- kas/kas/migrations/0003_groups.py
+++ kas/kas/migrations/0003_groups.py
@@ -1,4 +1,7 @@
 import os
+
+from django.contrib.auth.models import Permission
+from django.core.management.sql import emit_post_migrate_signal
 from django.db import migrations
 
 
@@ -10,6 +13,21 @@ class Migration(migrations.Migration):
     def create_groups(apps, schema_editor):
         from django.contrib.auth.models import Group
         group, _ = Group.objects.get_or_create(name=os.environ['DJANGO_ADMIN_GROUP'])
+
+        # Send signal that will trigger Permission object creation
+        emit_post_migrate_signal(2, False, 'default')
+
+        kas_permissions = Permission.objects.filter(content_type__app_label='kas')
+        group.permissions.set([
+            kas_permissions.get(codename='add_pensioncompany'),
+            kas_permissions.get(codename='change_pensioncompany'),
+
+            kas_permissions.get(codename='add_persontaxyear'),
+            kas_permissions.get(codename='change_persontaxyear'),
+
+            kas_permissions.get(codename='add_policytaxyear'),
+            kas_permissions.get(codename='change_policytaxyear'),
+        ])
 
     operations = [
         migrations.RunPython(create_groups),
