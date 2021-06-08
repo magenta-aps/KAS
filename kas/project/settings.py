75e87b37e633d0e3108449758fbd0e75e2aedc6a
Lars Peter Thomsen <larsp@magenta-aps.dk>
2021-06-08 07:34:45 +0000

    [#43518] manual

--- kas/project/settings.py
+++ kas/project/settings.py
@@ -1,5 +1,6 @@
 import os
 from distutils.util import strtobool
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,6 +119,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+LANGUAGES = [
+    ('da', _('Danish')),
+    ('kl', _('Greenlandic')),
+]
+
 
 STATIC_URL = '/static/'
 
