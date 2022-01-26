dc5080cd1a94ece0c17af7f827718d4fc1a752fe
Mads Møller Johansen <mmj@magenta.dk>
2022-01-26 16:44:52 +0100

    48135: fix during review

--- kas/project/dafo.py
+++ kas/project/dafo.py
@@ -10,23 +10,21 @@ class DatafordelerClient(object):
                  service_header_cpr=None, uxp_service_owned_by=None,
                  certificate=None, private_key=None, url=None, root_ca=True, timeout=60):
 
-        self._mock = mock
-
-        if not self._mock:
-            self._client_header = client_header
-            self._service_header_cpr = service_header_cpr
-            self._uxp_service_owned_by = uxp_service_owned_by
-            self._cert = (certificate, private_key)
-            self._root_ca = root_ca
-            self._url = url
-            self._root_ca = root_ca
-            self._timeout = timeout
-            self._session = Session()
-            self._session.headers.update({'Uxp-Client': client_header})
+        self.mock = mock
+        self.client_header = client_header
+        self.service_header_cpr = service_header_cpr
+        self.uxp_service_owned_by = uxp_service_owned_by
+        self.cert = (certificate, private_key)
+        self.root_ca = root_ca
+        self.url = url
+        self.root_ca = root_ca
+        self.timeout = timeout
+        self.session = Session()
+        self.session.headers.update({'Uxp-Client': client_header})
 
     def __del__(self):
         if hasattr(self, '_session'):
-            self._session.close()
+            self.session.close()
 
     @classmethod
     def from_settings(cls):
@@ -36,7 +34,7 @@ class DatafordelerClient(object):
         """
         Lookup address information for cpr
         """
-        if self._mock:
+        if self.mock:
             return {"1111111111": {
                     "cprNummer": "1111111111",
                     "fornavn": "Anders",
@@ -57,7 +55,7 @@ class DatafordelerClient(object):
 
     def _get(self, number, service_header):
         url = urljoin(self._url, number)
-        r = self._session.get(url, cert=self._cert, verify=self._root_ca, timeout=self._timeout,
+        r = self.session.get(url, cert=self.cert, verify=self.root_ca, timeout=self.timeout,
                               headers={'Uxp-Service': service_header})
         r.raise_for_status()
         return r.json()
