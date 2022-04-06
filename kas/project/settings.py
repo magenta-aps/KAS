526004920aa0befe646cfd47b415ee66cbec3f34
Mads Møller Johansen <mmj@magenta.dk>
2022-04-06 12:15:31 +0200

    48973: Add comment

--- kas/project/settings.py
+++ kas/project/settings.py
@@ -12,6 +12,7 @@ TIME_ZONE = os.environ['DJANGO_TIMEZONE']
 LOGIN_REDIRECT_URL = '/'
 SESSION_COOKIE_NAME = 'admin-sessionid'
 WHITENOISE_USE_FINDERS = True
+# DEFAULT_AUTO_FIELD  er sat da vi benytter 3 parts biblioteker der ikke har fuldt implementeret app_config
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
