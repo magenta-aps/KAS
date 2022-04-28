f6640c6e46d8fefbaf590ebfcfbf9c229b6234dc
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2022-04-28 13:23:52 +0200

    ensure we do not override data in GPS #49993

--- kas/kas/management/commands/create_dummy_users.py
+++ kas/kas/management/commands/create_dummy_users.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
     help = 'Creates dummy users'
 
     def handle(self, *args, **options):
-        if settings.ENVIRONMENT == "production":
-            raise Exception("Will not create dummy users in production")
+        if settings.ENVIRONMENT in ("production", 'staging'):
+            raise Exception(f"Will not create dummy users in {settings.ENVIRONMENT}")
 
         User.objects.update_or_create(
             defaults={'is_superuser': True,
