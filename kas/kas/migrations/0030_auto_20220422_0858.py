58790791773e7a81fc5cc436fc6669af595dd791
Mads Møller Johansen <mmj@magenta.dk>
2022-05-23 08:23:21 +0200

    49835: refactor

--- kas/kas/migrations/0030_auto_20220422_0858.py
+++ kas/kas/migrations/0030_auto_20220422_0858.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('kas', '0029_auto_20220328_0920'),
+        ('kas', '0031_migrate_files'),
     ]
 
     operations = [
