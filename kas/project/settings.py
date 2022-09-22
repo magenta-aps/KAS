7051b390be6241216066ec3f6f269c8866c6c814
Lars Peter Thomsen <larsp@magenta-aps.dk>
2022-09-22 12:54:13 +0200

    [#52493] Get version from env

--- kas/project/settings.py
+++ kas/project/settings.py
@@ -3,6 +3,8 @@ from distutils.util import strtobool
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ImproperlyConfigured
 
+VERSION = os.environ["VERSION"]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
