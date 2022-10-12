da25fa4fe9a3a1f437a3ba76543f600cc81e64ef
Peter Juelsgaard <pj@magenta.dk>
2022-10-12 14:55:15 +0200

    Black linting

--- kas/kas/reportgeneration/kas_final_statement.py
+++ kas/kas/reportgeneration/kas_final_statement.py
@@ -385,7 +385,11 @@ class TaxFinalStatementPDF(FPDF):
         :param language:
         :return:
         """
-        person_name = self._person_tax_year.person.name if self._person_tax_year.person.name else " "
+        person_name = (
+            self._person_tax_year.person.name
+            if self._person_tax_year.person.name
+            else " "
+        )
         self.add_page()
         self.page_counter = 1
         self.set_fill_color(180, 180, 180)
@@ -423,9 +427,7 @@ class TaxFinalStatementPDF(FPDF):
             3,
             align="L",
             border=0,
-            txt=person_name
-            + "\n"
-            + self._person_tax_year.person.postal_address,
+            txt=person_name + "\n" + self._person_tax_year.person.postal_address,
         )
 
         # Adressing department
