b6d28d2db8529c8ad779eb142dc688c739127b61
Lars Peter Thomsen <larsp@magenta-aps.dk>
2023-01-05 15:59:02 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- kas/kas/models.py
+++ kas/kas/models.py
@@ -2027,7 +2027,7 @@ class FinalSettlement(EboksDispatch):
 
         total_tax = sum(
             [
-                policy.get_calculation()["tax_to_pay"]
+                policy.get_calculation()["tax_to_pay"]  # Samme som tax_with_deductions, men med beløb under 100 kr nulstillet
                 for policy in self.person_tax_year.active_policies_qs
                 if not policy.pension_company_pays
             ]
