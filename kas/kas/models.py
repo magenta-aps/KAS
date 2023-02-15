210b7a88cc2a2c440ee9ca45c1dda2da7c6bb2d6
Peter Juelsgaard <pj@magenta.dk>
2023-02-15 16:13:32 +0100

    The program works now. There is still some cleanup to do in the templates, and unnecessary comments and templates must be deleted

--- kas/kas/models.py
+++ kas/kas/models.py
@@ -1036,6 +1036,29 @@ class PolicyTaxYear(HistoryMixin, models.Model):
         return policy_qs.order_by("-history_date")[0]
 
     @property
+    def policy_at_final_settlement(self):
+        """
+        If policy is involved in a final settlement, returns the version of policy
+        that was valid, when the final settlement was generated
+        """
+        final_settlement_creation_date = FinalSettlement.objects.filter(
+            person_tax_year__person__id=self.person_tax_year.person.id,
+            person_tax_year__tax_year__year=self.person_tax_year.tax_year.year,
+        )
+        if not final_settlement_creation_date:
+            return None
+        final_settlement_creation_date = final_settlement_creation_date.order_by(
+            "-created_at"
+        )[0].created_at
+        policy_qs = self.history.filter(
+            history_date__lte=final_settlement_creation_date
+        )
+        if not policy_qs:
+            return None
+        return policy_qs.order_by("-history_date")[0]
+        
+
+    @property
     def reported_difference(self):
         if self.self_reported_amount is None or self.prefilled_adjusted_amount is None:
             return None
