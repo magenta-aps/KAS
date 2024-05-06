6779bc4a9e157abd727e79f888cc16ab6dc1bf11
Lars Peter Thomsen <larsp@magenta-aps.dk>
2024-05-06 14:24:17 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- kas/eskat/models.py
+++ kas/eskat/models.py
@@ -368,7 +368,7 @@ class ImportedKasMandtal(AbstractModels.KasMandtal):
             pk__in=list(
                 # We could use qs.values_list("pk", flat=True) here, but if
                 # qs and orphan_qs are not subject to the same filtering
-                # (e.g. qs is filtered more than delete_qs) we risk finding
+                # (e.g. qs is filtered more than orphan_qs) we risk finding
                 # too many objects in delete_qs and thus nuking the
                 # ImportedKasMandtal table. Better to be safe.
                 source_model.objects.filter(skatteaar=year).values_list("pk", flat=True)
