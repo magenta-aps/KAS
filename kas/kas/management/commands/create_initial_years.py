a66c33511a8419de02ee23247ce9e28eab6e61df
Lars Peter Thomsen <larsp@magenta-aps.dk>
2024-06-17 10:38:25 +0200

    fix: suggestion

--- kas/kas/management/commands/create_initial_years.py
+++ kas/kas/management/commands/create_initial_years.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        for year in (2018, 2019, 2020, 2021, 2022, 2023, 2024):
+        for year in (2018, 2019, 2020, 2021):
             year_part = "selvangivelse"
 
             # Older years should default to genoptagelsesperiode
