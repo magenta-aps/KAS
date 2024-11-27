bc697eb5418fab82ad5c77cff075e20284566e24
Lars Peter Thomsen <larsp@magenta.dk>
2024-11-27 12:59:46 +0100

    chore: [#63048] No fuzzy messages

--- kas/kas/management/commands/make_messages.py
+++ kas/kas/management/commands/make_messages.py
@@ -6,4 +6,6 @@ from django.core.management.commands import makemessages
 
 
 class Command(makemessages.Command):
-    msgmerge_options = list({*makemessages.Command.msgmerge_options, "--no-fuzzy-matching"})
+    msgmerge_options = list(
+        {*makemessages.Command.msgmerge_options, "--no-fuzzy-matching"}
+    )
