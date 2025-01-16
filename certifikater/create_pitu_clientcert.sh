527f198c7fd1a70534b547308e9f488c44a9dc79
Lars Peter Thomsen <larsp@magenta.dk>
2025-01-16 17:16:22 +0100

    feat: [#63582] Script til at oprette nye pitu certs

--- certifikater/create_pitu_clientcert.sh
+++ certifikater/create_pitu_clientcert.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 openssl req -x509 -newkey rsa:4096 -keyout kas_pitu.key -out kas_pitu.crt -sha256 -days 730 -nodes -subj "/C=GL/ST=Sermersooq/L=Nuuk/O=Magenta ApS/CN=KAS Pitu Client"
 echo "Oprettede kas_pitu.crt og kas_pitu.key"
 echo "Gå ind i PITU -> PITU : GOV : DIA : kas -> Internal TLS Certificates -> add, og upload kas_pitu.crt"
