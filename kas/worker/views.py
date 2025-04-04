74de910a4fa07e6c7fe8d9870410b1e442e32f44
Lars Peter Thomsen <larsp@magenta.dk>
2025-04-04 13:29:28 +0200

    fix: [#64619] Further filtering of HTMX joblist

--- kas/worker/views.py
+++ kas/worker/views.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
@@ -21,13 +23,28 @@ class JobListHtmxView(KasMixin, PermissionRequiredMixin, ListView):
     permission_required = "worker.view_job"
 
     def get_queryset(self):
+        chunk_size = 10
         last_uuid = self.kwargs.get("last_uuid")
         qs = Job.objects.filter(parent__isnull=True)
         if last_uuid:
-            # if you provide a none existing uuid you get a 404
+            # if you provide a nonexisting uuid you get a 404
             last_object = get_object_or_404(Job, uuid=last_uuid)
-            qs = qs.filter(created_at__lt=last_object.created_at)
-        return qs.order_by("-created_at")[:10]
+
+            # For some reason this doesn't work completely,
+            # it outputs Jobs that are later than last_object,
+            # when objects are less than one second apart
+            qs = qs.filter(created_at__lt=last_object.created_at).order_by("-created_at")
+
+            # Do further filtering
+            return islice(
+                filter(
+                    lambda job: job.created_at < last_object.created_at,
+                    qs.iterator()
+                ),
+                chunk_size
+            )
+
+        return qs.order_by("-created_at")[:chunk_size]
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(
