6e34e6aae9482816b7190697f92790c39ef00300
Peter Juelsgaard <pj@magenta.dk>
2025-09-09 14:52:32 +0200

    chore: [#66355] Upgrade Postgres

--- selvbetjening/project/settings/database.py
+++ selvbetjening/project/settings/database.py
@@ -3,9 +3,9 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["SELVBETJENING_DB"],
+        "USER": os.environ["SELVBETJENING_DB_USER"],
+        "PASSWORD": os.environ["SELVBETJENING_DB_PASSWORD"],
+        "HOST": os.environ["SELVBETJENING_DB_HOST"],
     }
 }
