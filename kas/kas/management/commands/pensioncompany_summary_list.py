09fd5e82eb46677e0351efd61207225eeb76e5bd
Peter Juelsgaard <pj@magenta-aps.dk>
2026-06-03 15:52:39 +0200

    fix: [#70354] Command to generate an excel file summarizing PensionCompanies

---
+++ kas/kas/management/commands/pensioncompany_summary_list.py
@@ -0,0 +1,77 @@
+import numpy as np
+from django.conf import settings
+from django.core.management import BaseCommand
+from pandas import DataFrame, Series
+
+from kas.models import PensionCompany, PolicyTaxYear
+
+
+class Command(BaseCommand):
+    help = """
+        Calculates a summarized overview for all PensionCompanies
+        and returns a .csv table
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("year", type=int)
+
+    def handle(self, *args, **options):
+        year = options["year"]
+        pensioncompanies = PensionCompany.objects.filter(
+            policytaxyear__person_tax_year__tax_year__year=year,
+        ).distinct()
+        dtypes = {
+            "Pensionskasse": str,
+            "Antal": float,
+            "Afkast": float,
+            "Modregnet negativt afkast tidl. år": float,
+            "Beregningsgrundlag": float,
+            "Forudbetaling": float,
+            "Kapitalafkastskat": float,
+        }
+        columns = list(dtypes.keys())
+        df = DataFrame(
+            np.nan,
+            index=range(len(pensioncompanies)),
+            columns=columns,
+        ).astype(dtypes)
+
+        for idx, pc in enumerate(pensioncompanies):
+            ptys = PolicyTaxYear.objects.filter(
+                pension_company=pc,
+                person_tax_year__tax_year__year=year,
+            )
+            pty_calcs = [
+                {
+                    "preliminary_paid_amount": pty.preliminary_paid_amount,
+                    "foreign_paid_amount_actual": pty.foreign_paid_amount_actual,
+                    **pty.get_calculation(),
+                }
+                for pty in ptys
+            ]
+            if not pty_calcs:
+                continue
+
+            pc_df = DataFrame(pty_calcs)
+            pc_df["prepaid"] = (
+                pc_df["preliminary_paid_amount"] + pc_df["foreign_paid_amount_actual"]
+            )
+            row = Series(
+                {
+                    "Pensionskasse": pc.name,
+                    "Antal": ptys.values("person_tax_year__person").distinct().count(),
+                    "Afkast": pc_df["year_adjusted_amount"].sum(),
+                    "Modregnet negativt afkast tidl. år": pc_df[
+                        "used_negative_return"
+                    ].sum(),
+                    "Beregningsgrundlag": pc_df["taxable_amount"].sum(),
+                    "Forudbetaling": pc_df["prepaid"].sum(),
+                    "Kapitalafkastskat": pc_df["tax_to_pay"].sum(),
+                }
+            )
+            df.loc[idx] = row
+        df.dropna(how="all", inplace=True)
+        df.to_csv(
+            settings.MEDIA_ROOT + "pensioncompany_summary_list_" + str(year) + ".xlsx",
+            index=False,
+        )
